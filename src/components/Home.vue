7d1b32c4e5ff9c7ccb5176aea8937b611f1eed4f
baldrani <mael.mayon@free.fr>
2018-07-31 23:29:40 +0200

    First commit :rocket:

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fluid>
+    <v-layout column>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <h1>Landing page</h1>
+        <p>Home</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {}
+</script>
